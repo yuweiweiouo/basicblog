279ff3f460f6c93a068d27d40ad3cc2484f77965
yuweiwei <yy80072312@gmail.com>
2017-09-15 17:22:35 +0800

    ok

--- js/db.js
+++ js/db.js
@@ -9,16 +9,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
-firebase.auth().createUserWithEmailAndPassword(account.value, pwd.value).then(function() {
-    //登入成功後，取得登入使用者資訊
+var database = firebase.database();
 
-    loginUser = firebase.auth().currentUser;
-    console.log("登入使用者為", loginUser);
-    firebase.database().ref('users/' + loginUser.uid).set({
-        email: loginUser.email,
-        name: name.value,
-        age: age.value
-    }).catch(function(error) {
-        console.error("寫入使用者資訊錯誤", error);
-    });
-})
+var dbRef = firebase.database().ref('test/');
+
+console.log("ok");
