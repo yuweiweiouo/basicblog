5c8a7e1b67d999fa691535b0c8c1805b70118993
yuweiweiouo <yy80072312@gmail.com>
2017-09-16 01:05:35 +0800

    no

---
+++ js/loading.js
@@ -0,0 +1,75 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDIvGlbeFCi3Jo3I9-uNv5qqjHu-qONd3w",
+    authDomain: "my-project-1492012958680.firebaseapp.com",
+    databaseURL: "https://my-project-1492012958680.firebaseio.com",
+    projectId: "my-project-1492012958680",
+    storageBucket: "",
+    messagingSenderId: "377007134663"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var articleRef = firebase.database().ref('article').once('value');
+
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function(child) {
+        var item = child.val();
+        returnArr.push(item);
+    });
+    return returnArr;
+}
+
+function loadArticle() {
+    articleRef.then(function(snapshot) {
+        var articles = snapshotToArray(snapshot);
+        var artArr = [];
+        articles.forEach(function(article) {
+            var oneOfArticle = `
+			<div class="article">
+			<div class="article-title">
+			<div class="article-title-icon">
+			<img src="./img/title.png">
+			</div>
+			<div class="article-title-txt">
+			${article.title}
+			</div>
+			</div>
+			<div class="article-content">
+			<img src="${article.img}">
+			<div class="article-content-txt">
+			${article.content}
+			</div>
+			</div>
+			<div class="article-bottom">
+			<div class="article-bottom-l">
+			<img src="./img/calendar.png"> 
+			${article.date}
+			</div>
+			<div class="article-bottom-r">
+			<img src="./img/more.png">
+			</div>
+			</div>
+			</div>
+			`;
+            artArr.push(oneOfArticle);
+        });
+        artArr.forEach(function(a) {
+            $(".-home").append(a);
+        });
+    });
+}
+
+loadArticle();
+
+function login(user) {
+	localStorage.setItem('Login','true');
+    firebase.auth().signInWithEmailAndPassword(user.account, user.pwd).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("登入失敗:" + errorCode + "\n訊息:" + errorMessage);
+        localStorage.setItem('Login','false');
+    });
+}
